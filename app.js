897dd09c13c57d16daadfce10a04203c39406ba6
Your Name <you@example.com>
2023-08-12 16:00:20 +1200

    upload vercel.json

--- app.js
+++ app.js
@@ -26,3 +26,6 @@ app.use('/api/books', books);
 const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+const path = require("path")
+process.env['NODE_CONFIG_DIR'] = path.join(path.resolve("./"),"config/")
